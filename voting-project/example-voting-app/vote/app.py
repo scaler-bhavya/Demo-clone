af785da93d90a7b2d265cf29487ae43db99f2c98
scaler-bhavya <bhavya.sachdeva@scaler.com>
2025-12-01 16:53:45 +0530

    Change default voting options to 'cats' and 'dogs'

--- voting-project/example-voting-app/vote/app.py
+++ voting-project/example-voting-app/vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "red")
-option_b = os.getenv('OPTION_B', "blue")
+option_a = os.getenv('OPTION_A', "cats")
+option_b = os.getenv('OPTION_B', "dogs")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
